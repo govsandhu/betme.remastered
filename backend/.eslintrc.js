f1ceffd87afb8dd31b3e3a35f2c06bcd5659eae0
Govind Sandhu <Govind@govindsndhusmbp.lan>
2020-04-23 00:48:43 -0700

    Project setup.

---
+++ backend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  rules: {
+    'array-bracket-newline': ['warn', 'consistent'],
+    'array-element-newline': ['warn', 'consistent'],
+    'comma-dangle': ['warn', 'never'],
+    'comma-spacing': ['warn', { before: false, after: true }],
+    'max-len': ['warn', 180],
+    'no-multiple-empty-lines': ['warn', { max: 1, maxEOF: 1, maxBOF: 0 }],
+    'no-trailing-spaces': ['warn', { skipBlankLines: true }],
+    'object-curly-newline': ['warn', { consistent: true }],
+    'object-curly-spacing': ['warn', 'always'],
+    'object-property-newline': ['warn', 'never']
+  }
+};
