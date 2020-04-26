054f53259b1395a1804dc2f9e64f20806ec77496
Govind Sandhu <Govind@govindsndhusmbp.lan>
2020-04-26 16:26:50 -0700

    More eslint cleanup.

--- betme.web/.eslintrc.js
+++ betme.web/.eslintrc.js
@@ -2,22 +2,46 @@ module.exports = {
   env: {
     node: true,
     es6: true,
-    browser: true,
+    browser: true
   },
   extends: [
     'plugin:vue/recommended',
     '@vue/airbnb',
     '@vue/typescript',
     'plugin:@typescript-eslint/recommended',
-    'plugin:import/typescript',
+    'plugin:import/typescript'
   ],
   rules: {
-
+    'vue/singleline-html-element-content-newline': ['off', {
+      ignoreWhenNoAttributes: true,
+      ignoreWhenEmpty: true
+    }],
+    'vue/max-attributes-per-line': [
+      'error', {
+        singleline: 5,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ],
+    'array-bracket-newline': ['warn', 'consistent'],
+    'array-element-newline': ['warn', 'consistent'],
+    'comma-dangle': ['warn', 'never'],
+    'comma-spacing': ['warn', { before: false, after: true }],
+    'max-len': ['warn', 180],
+    'no-multiple-empty-lines': ['warn', { max: 1, maxEOF: 1, maxBOF: 0 }],
+    'no-trailing-spaces': ['warn', { skipBlankLines: true }],
+    'object-curly-newline': ['warn', { consistent: true }],
+    'object-curly-spacing': ['warn', 'always'],
+    'quote-props': ['warn', 'consistent-as-needed'],
+    'semi': ['error', 'always'],
+    'semi-style': ['error', 'last']
   },
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
-    ecmaVersion: 2018,
-  },
+    ecmaVersion: 2018
+  }
 };
